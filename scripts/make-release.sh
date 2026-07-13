fd4cc92438a524a23f2114544441a4819a182180
Mitrovic Sasa <simcomp2003@gmail.com>
2026-07-13 11:10:06 +0200

    Make zip release

--- scripts/make-release.sh
+++ scripts/make-release.sh
@@ -3,15 +3,15 @@
 PKG_VERSION=$(cat ./package.json | jq -r ".version")
 # First, clean the project and prevous releases
 echo "Removing old files..."
-rm -rf ./dist/ ./node_modules/ ./*.tar.gz
+rm -rf ./dist/ ./node_modules/ ./*.zip
 # Compile it again
 echo "Compiling code..."
 npm ci && npm run build && mage -v
 # Make distribution files
 echo "Compacting release files..."
 cd ./dist/
-tar --exclude="gpx*arm*" -cvzf ../albertowd-oraclegrafana-datasource-bundle-${PKG_VERSION}.tar.gz ./
-tar --exclude="gpx*arm*" --exclude="gpx*linux*" --exclude="gpx*windows*" -cvzf ../albertowd-oraclegrafana-datasource-darwin-amd64-${PKG_VERSION}.tar.gz ./
-tar --exclude="gpx*arm*" --exclude="gpx*darwin*" --exclude="gpx*windows*" -cvzf ../albertowd-oraclegrafana-datasource-linux-amd64-${PKG_VERSION}.tar.gz ./
-tar --exclude="gpx*arm*" --exclude="gpx*darwin*" --exclude="gpx*linux*" -cvzf ../albertowd-oraclegrafana-datasource-windows-amd64-${PKG_VERSION}.tar.gz ./
+zip -r ../albertowd-oraclegrafana-datasource-bundle-${PKG_VERSION}.zip . --exclude="gpx*arm*"
+zip -r ../albertowd-oraclegrafana-datasource-darwin-amd64-${PKG_VERSION}.zip . --exclude="gpx*arm*" --exclude="gpx*linux*" --exclude="gpx*windows*"
+zip -r ../albertowd-oraclegrafana-datasource-linux-amd64-${PKG_VERSION}.zip . --exclude="gpx*arm*" --exclude="gpx*darwin*" --exclude="gpx*windows*"
+zip -r ../albertowd-oraclegrafana-datasource-windows-amd64-${PKG_VERSION}.zip . --exclude="gpx*arm*" --exclude="gpx*darwin*" --exclude="gpx*linux*"
 cd ../
